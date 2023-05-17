34cc92934e6608f6c07f7f70a5fefbadc312eb0e
SC-Michelle <wrightmichelle13@gmail.com>
2023-05-17 19:40:43 -0400

    Ops Challenge 13

--- cc13.sh
+++ cc13.sh
@@ -8,9 +8,9 @@
 echo "Type a website"
 read website
 
-whois $website > DomainAnalyzer.txt
+whois $website >> DomainAnalyzer.txt
 dig $website >> DomainAnalyzer.txt
-hots $website >> DomainAnalyzer.txt
+host $website >> DomainAnalyzer.txt
 nslookup $website >> DomainAnalyzer.txt
 
 #End
