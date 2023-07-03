0136d9b579979101f85065df45a447c590939d5c
Michelle Wright <129065695+SC-Michelle@users.noreply.github.com>
2023-07-03 12:14:27 +0000

    Make up 1

---
+++ cc0305.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get the current time
+timestamp=$(date +"%Y%m%d%H%M%S")
+
+# Get the size of the original log files
+syslog_size=$(stat -c%s /var/log/syslog)
+wtmp_size=$(stat -c%s /var/log/wtmp)
+
+# Compress the log files
+gzip -c /var/log/syslog > /var/log/backups/syslog-$timestamp.gz
+gzip -c /var/log/wtmp > /var/log/backups/wtmp-$timestamp.gz
+
+# Clear the contents of the log files
+> /var/log/syslog
+> /var/log/wtmp
+
+# Get the size of the compressed log files
+syslog_compressed_size=$(stat -c%s /var/log/backups/syslog-$timestamp.gz)
+wtmp_compressed_size=$(stat -c%s /var/log/backups/wtmp-$timestamp.gz)
+
+# Compare the sizes of the original and compressed log files
+echo "Original syslog file size: $syslog_size"
+echo "Compressed syslog file size: $syslog_compressed_size"
+echo "Original wtmp file size: $wtmp_size"
+echo "Compressed wtmp file size: $wtmp_compressed_size"
